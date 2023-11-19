a3c2e7bf91880e9fd59566e5b1a8f775d2d6978d
koke1997 <67540157+koke1997@users.noreply.github.com>
2023-11-19 17:30:44 +0100

    Small patch for withdraw
    Added traceback logger to transaction routes
    Added more loggers around transactions routes

--- FiatHandling/withdraw.py
+++ FiatHandling/withdraw.py
@@ -1,27 +1,67 @@
-from DatabaseHandling.connection import connect_db
+#withdraw.py
+
+import mysql.connector
+from DatabaseHandling.connection import connect_db, get_db_cursor
+from validation_utils import validate_account
+import logging
+
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def withdraw(user_id, amount):
-    connection = connect_db()
-    cursor = connection.cursor()
+    """
+    This function handles the withdrawal of funds for a user.
+    """
 
-    # Ensure the user has sufficient funds
-    balance_query = "SELECT balance FROM Accounts WHERE user_id = %s"
-    cursor.execute(balance_query, (user_id,))
-    result = cursor.fetchone()
-    if not result or result[0] < amount:
-        return "Insufficient funds"
+    # Establish a database connection and get a cursor
+    db = connect_db()
+    cursor = get_db_cursor(db)
 
-    # Update the account balance
-    withdraw_query = "UPDATE Accounts SET balance = balance - %s WHERE user_id = %s"
-    cursor.execute(withdraw_query, (amount, user_id))
+    try:
+        logger.info(f"Attempting withdrawal: User ID {user_id}, Amount {amount}")
 
-    # Log the transaction
-    log_query = """INSERT INTO Transactions (from_account_id, amount, transaction_type, transaction_status) 
-                   VALUES (%s, %s, 'Withdrawal', 'Completed')"""
-    cursor.execute(log_query, (user_id, amount))
+        # Fetch the account_id for the given user_id
+        account_query = "SELECT account_id FROM accounts WHERE user_id = %s"
+        cursor.execute(account_query, (user_id,))
+        account = cursor.fetchone()
 
-    connection.commit()
-    cursor.close()
-    connection.close()
+        if not account:
+            logger.warning(f"Account not found for user {user_id}")
+            return "Account not found for user"
 
-    return "Withdrawal successful"
+        account_id = account[0]
+
+        if not validate_account(account_id):
+            logger.warning(f"Invalid account: Account ID {account_id}")
+            return "Invalid account"
+
+        # Check if the user has sufficient funds
+        cursor.execute("SELECT balance FROM accounts WHERE account_id = %s", (account_id,))
+        balance = cursor.fetchone()[0]
+
+        if balance < amount:
+            logger.warning(f"Insufficient balance for withdrawal: User ID {user_id}, Balance {balance}, Amount {amount}")
+            return "Insufficient balance for withdrawal"
+
+        # Update balance
+        update_query = "UPDATE accounts SET balance = balance - %s WHERE account_id = %s"
+        cursor.execute(update_query, (amount, account_id))
+
+        # Log the transaction
+        log_query = """INSERT INTO transactions (from_account_id, amount, transaction_type, description) 
+                       VALUES (%s, %s, 'withdrawal', 'Withdrawal from account')"""
+        cursor.execute(log_query, (account_id, amount))
+
+        db.commit()
+        logger.info(f"Withdrawal successful: User ID {user_id}, Amount {amount}")
+
+        return "Withdrawal successful"
+
+    except mysql.connector.Error as err:
+        logger.error(f"Error during withdrawal: {err}")
+        db.rollback()
+        return f"An error occurred: {err}"
+    finally:
+        cursor.close()
+        db.close()
--- routes/transaction_routes.py
+++ routes/transaction_routes.py
@@ -1,13 +1,25 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash, session
+from flask import Flask, Blueprint, render_template, request, redirect, url_for, flash, session
 from DatabaseHandling.connection import connect_db, get_db_cursor
 import logging
+import psycopg2.errors
+import traceback  # Import the traceback module
 
-# Initialize logging
-logging.basicConfig(level=logging.INFO)
+# Initialize Flask app
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Replace with your actual secret key
+
+# Initialize logging with a custom log format
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+# Define a Blueprint for transaction routes
 transaction_routes = Blueprint('transaction_routes', __name__)
 
+# Define your other routes and configurations here (snippets you have)
+
+# ...
+
+# Define the deposit route
 @transaction_routes.route('/deposit', methods=['GET', 'POST'])
 def deposit():
     if request.method == 'POST':
@@ -44,7 +56,7 @@ def deposit():
 
     return render_template('deposit.html')
 
-
+# Define the withdraw route
 @transaction_routes.route('/withdraw', methods=['GET', 'POST'])
 def withdraw():
     if request.method == 'POST':
@@ -53,42 +65,62 @@ def withdraw():
             account_id = session.get('selected_account_id')
 
             conn, cursor = get_db_cursor()
+
             # Ensure the account has sufficient balance
             cursor.execute("SELECT balance FROM accounts WHERE account_id = %s", (account_id,))
-            balance = cursor.fetchone()[0]
-            if balance >= withdrawal_amount:
-                # Log the SQL query
-                update_query = "UPDATE accounts SET balance = balance - %s WHERE account_id = %s"
-                cursor.execute(update_query, (withdrawal_amount, account_id))
-                logger.info(f'Executed SQL query: {update_query} with parameters: ({withdrawal_amount}, {account_id})')
+            result = cursor.fetchone()  # Get the result
 
-                # Log the SQL query
-                insert_query = """
-                    INSERT INTO transactions (from_account_id, amount, transaction_type, description) 
-                    VALUES (%s, %s, 'withdrawal', 'Withdrawal from account')
-                    """
-                cursor.execute(insert_query, (account_id, withdrawal_amount))
-                logger.info(f'Executed SQL query: {insert_query} with parameters: ({account_id}, {withdrawal_amount})')
+            logger.info(f"Result of query: {result}")  # Add this line for debugging
 
-                conn.commit()
-                cursor.close()
-                conn.close()
+            if result is not None and 'balance' in result:  # Check if 'balance' key is in the result
+                balance = result['balance']  # Access the balance from the result dictionary
+                if balance >= withdrawal_amount:
+                    # Log the SQL query for updating balance
+                    update_query = "UPDATE accounts SET balance = balance - %s WHERE account_id = %s"
+                    cursor.execute(update_query, (withdrawal_amount, account_id))
+                    logger.info(f'Executed SQL query: {update_query} with parameters: ({withdrawal_amount}, {account_id})')
 
-                logger.info(f'Withdrawal successful! Amount: {withdrawal_amount}, Account ID: {account_id}')
-                flash('Withdrawal successful!', 'success')
+                    # Log the SQL query for inserting transaction
+                    insert_query = """
+                        INSERT INTO transactions (from_account_id, amount, transaction_type, description) 
+                        VALUES (%s, %s, 'withdrawal', 'Withdrawal from account')
+                        """
+                    cursor.execute(insert_query, (account_id, withdrawal_amount))
+                    logger.info(f'Executed SQL query: {insert_query} with parameters: ({account_id}, {withdrawal_amount})')
+
+                    conn.commit()
+                    cursor.close()
+                    conn.close()
+
+                    logger.info(f'Withdrawal successful! Amount: {withdrawal_amount}, Account ID: {account_id}')
+                    flash('Withdrawal successful!', 'success')
+                else:
+                    logger.warning(f'Insufficient balance for withdrawal! Amount: {withdrawal_amount}, Account ID: {account_id}')
+                    flash('Insufficient balance for withdrawal!', 'error')
             else:
-                logger.warning(f'Insufficient balance for withdrawal! Amount: {withdrawal_amount}, Account ID: {account_id}')
-                flash('Insufficient balance for withdrawal!', 'error')
+                logger.warning(f'Account not found or balance key missing! Account ID: {account_id}')
+                flash('Account not found or balance key missing!', 'error')
+        except psycopg2.errors.DatabaseError as db_error:
+            # Log the specific database error
+            logger.error(f"Database error during withdrawal: {db_error}")
+            flash(f"Database error during withdrawal: {db_error}", "error")
         except Exception as e:
-            # Log the specific error message
+            # Log any other exceptions
             logger.error(f"An error occurred during withdrawal: {str(e)}")
             flash(f"An error occurred during withdrawal: {str(e)}", "error")
+            print(f"Error details: {e}")  # Add this line for debugging
+            traceback.print_exc()  # Print the full traceback
 
         return redirect(url_for('account_routes.dashboard'))
 
     return render_template('withdraw.html')
 
 
+
+
+
+
+# Define the transfer route
 @transaction_routes.route('/transfer', methods=['GET', 'POST'])
 def transfer():
     if request.method == 'POST':
@@ -126,10 +158,20 @@ def transfer():
 
     return render_template('transfer.html')
 
+# Define the transaction history route
 @transaction_routes.route('/transaction_history', methods=['GET'])
 def transaction_history():
     # Logic to show transaction history
     return render_template('transaction_history.html')
 
+# Add other routes and configurations here (snippets you have)
+
+# ...
+
+# Configure the transaction routes in your app
 def configure_transaction_routes(app):
     app.register_blueprint(transaction_routes)
+
+if __name__ == '__main__':
+    configure_transaction_routes(app)
+    app.run(debug=True)
