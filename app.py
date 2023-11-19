004498d3145473ee69f50d0105f76b678db58774
koke1997 <67540157+koke1997@users.noreply.github.com>
2023-11-19 23:27:35 +0100

    Introducing API plotter
    python app.py will create a plot file of all API call that can be made to this app.

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-# app.py
 from app_factory import create_app
+import argparse
+from plot_diagram import plot_routes_diagram, extract_all_routes
 
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-diagram", action="store_true", help="Display routes diagram")
+    args = parser.parse_args()
+
+    if args.diagram:
+        extracted_routes = extract_all_routes()
+        plot_routes_diagram(extracted_routes)
+    else:
+        app = create_app()
+        app.run(debug=True)
---
+++ plot_diagram.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import os
+
+def plot_routes_diagram(extracted_routes):
+    G = nx.DiGraph()
+    for file, routes in extracted_routes.items():
+        for route in routes:
+            path = route.split("'")[1]  # Extracting the path
+            methods = route.split("'")[3]  # Extracting the HTTP methods
+            node_label = f"{path}\n({methods})"
+            G.add_node(node_label)
+            G.add_edge(file.replace('.py', ''), node_label)  # Connecting routes to their respective files
+
+    plt.figure(figsize=(12, 8))
+    pos = nx.spring_layout(G, seed=42)  # Positioning the nodes
+    nx.draw(G, pos, with_labels=True, node_color="skyblue", node_size=3000, edge_color="gray", linewidths=1, font_size=10)
+
+    plt.title("Visual Diagram of Routes in Project")
+    plt.show()
+
+def extract_routes_from_file(file_path):
+    routes = []
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            if '@' in line and 'route(' in line:
+                routes.append(line.strip())
+    return routes
+
+def extract_all_routes():
+    route_files = ['account_routes.py', 'transaction_routes.py', 'user_routes.py']
+    routes_directory_path = 'routes'  # Update this path to the correct one
+    extracted_routes = {}
+
+    for file_name in route_files:
+        file_path = os.path.join(routes_directory_path, file_name)
+        routes = extract_routes_from_file(file_path)
+        extracted_routes[file_name] = routes
+
+    return extracted_routes
